214b141484fb95daf6da3166ece847bc3f3d41f1
Mario Falkinowski Baeza <falkifn03177@gmail.com>
2025-10-01 19:19:41 +0200

    sdf

--- test1/lists.py
+++ test1/lists.py
@@ -128,9 +128,50 @@ def generate_serie_7(n:int):
     return last_number
 
 
+# Funcion que le das una lista y te devuelva el valor medio entre el meno y el mayor
 
-def middle_point(list):
-    if len(list) < 1:
+def smallest(list1):
+    if len(list1) < 1:
         return sys.maxsize
-    return list[len(list)//2]
-# Funcion que le das una lista y te devuelve el punto medio
+    i = 1
+    smallest = list1[0]
+    while i < len(list1):
+        if list1[i] < smallest:
+            smallest = list1[i]
+        i += 1
+    return smallest
+
+def greatest(list1):
+    if len(list1) < 1:
+        return sys.maxsize
+    greatest = list1[0]
+    i = 1
+    while i < len(list1):
+        if list1[i] > greatest:
+            greatest = list1[i]
+        i += 1
+    return greatest
+
+def middle_point(list1):
+    if len(list1) < 1:
+        return 0
+    return (smallest(list1) + greatest(list1)) / 2
+
+# Funcion que le das un numero y te devuelve la cantidad de veces ese numero al lista de fibonacci
+
+def fibonacci(n: int) -> int:
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+
+
+print(fibonacci(4))
+
+
+
+# Funcion que le das un numero y imprime la tabla de multiplicar de ese numero 
+# sin uar listas ejemplo: 7 devuelve 7x1=7, 7x2=14, 7x3=21...7x10=70
+
--- test1/main.py
+++ test1/main.py
@@ -5,7 +5,7 @@ import sys
 
 
 
-posicion = generate_serie_7(-3)
-print(posicion)
+# posicion = generate_serie_7(-3)
+# print(posicion)
 
-print(middle_point([1, 2, 3, 4, 5]))
+print(middle_point([1, 2, 3, 4, 14]))
