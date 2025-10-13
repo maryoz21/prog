782d43c190a1f1f78fccacc1bd1ec6b250bd3c14
Mario Falkinowski Baeza <falkifn03177@gmail.com>
2025-10-13 17:11:04 +0200

    jgtft

--- clases/main.py
+++ clases/main.py
@@ -1,11 +1,2 @@
-
-class GameObject:
-    name: str 
-    id: int
-
-obj1: GameObject = GameObject()
-obj2: GameObjetc = obj1
-
-obj1.id = 10
-obj2.id = 1
-obj2.id = 0
+from GameObject import GameObject
+from Typing import list
