db4ae9570ec3e70339bd4fe79067d28671c95c0e
Mario Falkinowski Baeza <falkifn03177@gmail.com>
2025-10-20 19:19:46 +0200

    dffdghfhf

---
+++ poker/PokerCard1.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+##########################
+# Types of value
+#-------------------------
+# 1: Diamonds
+# 2: Hearts
+# 3: Clubs
+# 4: Spades
+@dataclass
+class PokerCard:
+    number: int = -1
+    type: int = -1
+
+    def set_number(self, number):
+        if number is None:
+            return 
+        if number < 0:
+            return
+        if number > 13:
+            return 
+        self.number = number
+
+    def get_number(self) -> int:
+        return self.number
+    
+    def set_type(self, number):
+        if number is None:
+            return
+        if number < 1:
+            return
+        if number > 4:
+            return
+        self.type = number
+    
+    def get_type(self) -> int:
+        return self.type
+
+    def get_type_as_string(self) -> str:
+        if self.type == 1:
+            return "Diamonds"
+        if self.type == 2:
+            return "Hearts"
+        if self.type == 3:
+            return "Clubs"
+        if self.type == 4:
+            return "Spades"
+        return ""
+
+
+    def to_string(self) -> str:
+        return f"{self.get_number()} {self.get_type_as_string()}"
+
+card1 = PokerCard(10, 2)
+
+print(card1.get_type())
+print(card1.type)
+
+print(card1.to_string())
---
+++ poker/main.py
@@ -0,0 +1,2 @@
+from PokerCard1 import *
+
