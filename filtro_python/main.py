eceab95993dc087ddc21f79922e4908352e6bd70
Mario Falkinowski Baeza <falkifn03177@gmail.com>
2025-11-10 19:59:16 +0100

    dgfgdg

--- filtro_python/main.py
+++ filtro_python/main.py
@@ -1,3 +1,6 @@
+from wsgiref.handlers import CGIHandler
+
+from numpy import imag
 from image import *
 
 def gray_filter(image: Image) -> Image:
@@ -11,10 +14,57 @@ def gray_filter(image: Image) -> Image:
             c.g = media
             c.b = media
 
-            result.image.set_pixel(x, y, c)
+            result.set_pixel(x, y, c)
     return result
+
+def multiplicar_por_factores(image: Image, fr, fg, fb):
+    result: Image = Image(image.width, image.height)
+    for y in range(image.height):
+        for x in range(image.width):
+            c = image.get_pixel(x, y)
+
+            c.r *= fr
+            c.g *= fg
+            c.b *= fb
+            result.set_pixel(x, y, c)
+    return result
+
+def saturate(image: Image):
+    result = image
+    for y in range(image.height):
+        for x in range(image.width):
+            c = image.get_pixel(x,y)
+
+            if c.r < 0:
+                c.r = 0
+            if c.r > 1:
+                c.r = 1
+            
+            if c.g < 0:
+                c.g = 0
+            if c.g > 1:
+                c.g = 1
+
+            if c.b < 0:
+                c.b = 0
+            if c.b > 1:
+                c.b = 1
+    return result
+
+def get_negative(image: Image):
+    resultado = image
+    for y in range(image.height):
+        for x in range(image.width):
+            c = image.get_pixel(x,y)
+            c.r = 1 - c.r
+            c.g = 1-c.g
+            c.b = 1-c.b
+
+    return resultado
+
+
 
 img = Image()
 img.load_from("celda.tga")  # o .tga, .ppm, .png, .jpg
-res = gray_filter(img)
-res.save_to("salida1.tga")  # guarda en formato texto PPM
+res2 = get_negative(img)
+res2.save_to("salida.tga")  # guarda en formato texto PPM
