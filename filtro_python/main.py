815d711b631bac2139a0ff5a1a8f84ecb8a2c9de
Mario Falkinowski Baeza <falkifn03177@gmail.com>
2025-11-10 17:02:36 +0100

    dfgdfg

--- filtro_python/main.py
+++ filtro_python/main.py
@@ -1,19 +1,20 @@
 from image import *
 
+def gray_filter(image: Image) -> Image:
+    result: Image = Image(image.width, image.height)
+    for y in range (image.height):
+        for x in range(image.width):
+            c = image.get_pixel(x, y)
+
+            media = (c.r + c.b + c.g) / 3.0
+            c.r = media
+            c.g = media
+            c.b = media
+
+            result.image.set_pixel(x, y, c)
+    return result
 
 img = Image()
-img.load_from("coche.tga")  # o .tga, .ppm, .png, .jpg
-
-
-
-for y in range(img.height):
-    for x in range(img.width):
-        c = img.get_pixel(x, y)
-        hsl = c.to_hsl()
-        hsl.rotate(-0.25)
-        c = hsl.to_rgb()
-        img.set_pixel(x, y, c)
-
-
-
-img.save_to("salida.tga")  # guarda en formato texto PPM
+img.load_from("celda.tga")  # o .tga, .ppm, .png, .jpg
+res = gray_filter(img)
+res.save_to("salida1.tga")  # guarda en formato texto PPM
