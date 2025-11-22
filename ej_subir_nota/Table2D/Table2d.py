944e5fbdb8a5668a4e894d56d0be5aa475a82ae8
Mario Falkinowski Baeza <falkifn03177@gmail.com>
2025-11-22 19:50:42 +0100

    Refactor Table2D attribute names and add set_lista method
    Changed Table2D attributes to use double underscore for name mangling and updated all references accordingly. Added set_lista method to Table2D for resizing the internal list. Updated utils.py to use set_lista when converting an image to Table2D.

--- ej_subir_nota/Table2D/Table2d.py
+++ ej_subir_nota/Table2D/Table2d.py
@@ -1,28 +1,31 @@
 class Table2D:
     def __init__(self, ancho: int = 0, alto: int = 0):
-        self._ancho = ancho
-        self._alto = alto
-        self._lista = [0] * (ancho * alto)
+        self.__ancho = ancho
+        self.__alto = alto
+        self.__lista = [0] * (ancho * alto)
     
     def get_ancho(self) -> int:
-        return self._ancho
+        return self.__ancho
     
     def get_alto(self) -> int:
-        return self._alto
+        return self.__alto
 
     def get_cell(self, x, y) -> int:
         index = y * self._ancho + x
-        return self._lista[index] 
+        return self.__lista[index] 
 
     def get_cell_by_index(self, index) -> int:
-        return self._lista[index]
+        return self.__lista[index]
 
     def set_cell(self, x, y, value):
-        index = y * self._ancho + x
+        index = y * self.__ancho + x
         self._lista[index] = value
 
     def set_ancho(self, ancho):
-        self._ancho = ancho
+        self.__ancho = ancho
 
     def set_alto(self, alto):
-        self._alto = alto
+        self.__alto = alto
+    
+    def set_lista(self, tamaño):
+        self.__lista = [0] * tamaño
