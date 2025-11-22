f40a4d63b0291795a32ca94ddc60ba1b1a35d0c5
Mario Falkinowski Baeza <falkifn03177@gmail.com>
2025-11-22 20:36:37 +0100

    Add image convolution example and sample image
    Introduces a sample image 'pastillas.tga' and updates main.py to perform convolution using a sharpening kernel. The processed image is saved as 'pastillas_convolucion.tga', demonstrating image filtering functionality.

--- ej_subir_nota/kernel/main.py
+++ ej_subir_nota/kernel/main.py
@@ -1,6 +1,22 @@
 from turtle import color, width
 from unittest import result
 from wsgiref.handlers import CGIHandler
-from image import *
 from codec import *
+from image import Image
 from utils import *
+
+
+img = Image()
+img.load_from("pastillas.tga") 
+
+kernel = [
+    -1, -1, -1,
+    -1,  8, -1,
+    -1, -1, -1
+]
+
+img2 = convolution(img, kernel)
+
+img2.save_to("pastillas_convolucion.tga")
+
+print("Proceso terminado.")
