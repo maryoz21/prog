8c1ce5e189e5bebeb2f49b7e04e3b7b5db0d2789
Mario Falkinowski Baeza <falkifn03177@gmail.com>
2026-02-19 18:47:30 +0100

    Revert "Crear la estructura para el frontend"
    This reverts commit ddffef7cd638a9e0345fb802fc0c374ad58b63e4.

--- chess/Board.py
+++ chess/Board.py
@@ -17,12 +17,3 @@ class Board(ABC):
     def is_in_bounds(self, x: int, y: int) -> bool:
         pass
 
-    # En Board.py
-def to_dict(self):
-    # Esto debe devolver una matriz 8x8 o una lista de piezas
-    # Ejemplo simplificado:
-    return [
-        [piece.symbol if piece else "" for piece in row] 
-        for row in self.grid
-    ]
-
